d55ecf398427558fef0fa3d734d9c1a2db4ae83a
Chongt2 <t.m.chong16@gmail.com>
2020-01-15 03:13:08 -0500

    attempt to fix clear function

--- js/myScript.js
+++ js/myScript.js
@@ -1,7 +1,5 @@
 // remove text from message when clicking clear button
 function removeText() {
-  // document.getElementsByClassName("message").value() = "";
-  document.addEventListener(
-    "click", getELementsbyClassname("message").value() = "");
+  document.getElementById("message").value() = "";
   return;
 }
