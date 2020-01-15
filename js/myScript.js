eb4cdaa1c0401304f57a3de9694ed22053a37c22
Chongt2 <t.m.chong16@gmail.com>
2020-01-15 04:14:27 -0500

    test 2

--- js/myScript.js
+++ js/myScript.js
@@ -2,7 +2,12 @@
 // var x = document.getElementById("message").innerHTML;
 // function removeText(x) {
 function removeText(){
-  document.getElementById("message").innerHTML = "";
+  if(document.getElementById("message").innerHTML == ""){
+    document.getElementById("message").innerHTML = "Hello World!";
+  }
+  else{
+    document.getElementById("message").innerHTML = "";
+  }
   alert(document.getElementById("message").innerHTML == "");
   // if (x == "Hello World!"){
   //   x = "";
