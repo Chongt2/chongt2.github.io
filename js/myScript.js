951939dc77418ff6958d419d4d0f3be5be9fbc15
Chongt2 <t.m.chong16@gmail.com>
2020-01-15 03:37:06 -0500

    using outputs to test

--- js/myScript.js
+++ js/myScript.js
@@ -1,14 +1,15 @@
 // remove text from message when clicking clear
 var clearFlag = false;
-window.alert(clearFlag);
 function removeText(clearFlag) {
   if (clearFlag == false){
     document.getElementById("message").innerHTML = "";
     clearFlag = true;
+    window.alert(clearFlag);
   }
   else {
     document.getElementById("message").innerHTML = "Hello World!";
     clearFlag = false;
+    window.alert(clearFlag);
   }
   return;
 }
