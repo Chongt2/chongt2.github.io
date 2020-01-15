a0c40ed3f29259a73b5713eace890c1abdef0155
Chongt2 <t.m.chong16@gmail.com>
2020-01-15 03:21:26 -0500

    Help me Jesus

--- js/myScript.js
+++ js/myScript.js
@@ -1,5 +1,5 @@
 // remove text from message when clicking clear button
 function removeText() {
-  document.getElementById("message") = "";
+  document.getElementById("message").innerHTML = "";
   return;
 }
