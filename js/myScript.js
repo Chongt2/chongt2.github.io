1adb495e4592a15055203a33f8dceadd993765e8
Chongt2 <t.m.chong16@gmail.com>
2020-01-15 03:16:13 -0500

    Attempt to fix clear 2

--- js/myScript.js
+++ js/myScript.js
@@ -1,5 +1,5 @@
 // remove text from message when clicking clear button
 function removeText() {
-  document.getElementById("message").value() = "";
+  document.getElementById("message") = "";
   return;
 }
