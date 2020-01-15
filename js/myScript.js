93b4d9d4a73b0423be4c792bcd727379947c765d
Chongt2 <t.m.chong16@gmail.com>
2020-01-15 04:07:36 -0500

    fix 2

--- js/myScript.js
+++ js/myScript.js
@@ -2,10 +2,10 @@
 var x = document.getElementById("message").innerHTML;
 function removeText(x) {
   if (x == "Hello World!"){
-    document.getElementById("message").innerHTML = "";
+    x = "";
   }
   else {
-    document.getElementById("message").innerHTML = "Hello World!";
+    x = "Hello World!";
   }
   return;
 }
