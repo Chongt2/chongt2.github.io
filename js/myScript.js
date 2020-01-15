69fdfc0bbf99e2ea60848ef4cbf4e2d105965d16
Chongt2 <t.m.chong16@gmail.com>
2020-01-15 04:12:21 -0500

    test

--- js/myScript.js
+++ js/myScript.js
@@ -3,6 +3,7 @@
 // function removeText(x) {
 function removeText(){
   document.getElementById("message").innerHTML = "";
+  alert(document.getElementById("message").innerHTML == "");
   // if (x == "Hello World!"){
   //   x = "";
   // }
