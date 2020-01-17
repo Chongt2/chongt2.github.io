d37d6396f443ba2eaf954f60c9ce49a942bc3dc7
Tai-Yin Chong <t.m.chong16@gmail.com>
2020-01-16 21:02:03 -0500

    Add new color change button

--- js/myScript.js
+++ js/myScript.js
@@ -1,10 +1,15 @@
 // remove text from message when clicking
-function removeText(){
-  if(document.getElementById("message").innerHTML == ""){
+function removeText() {
+  if(document.getElementById("message").innerHTML == "") {
     document.getElementById("message").innerHTML = "Hello World!";
   }
-  else{
+  else {
     document.getElementById("message").innerHTML = "";
   }
+  return;
+}
+
+function changeColor() {
+  document.getElementById("message").style.color = "Red";
   return;
 }
